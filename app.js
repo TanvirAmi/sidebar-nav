836898910d5cebfec80a72bf99b8064ba6ebdb71
TanvirAmi <tanvir.focus@gmail.com>
2022-04-11 17:35:51 +0600

    added callback function when the button clicked

--- app.js
+++ app.js
@@ -3,5 +3,15 @@ const closeBtn = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 burgerMenu.addEventListener("click", function(){
-    console.log(sidebar.classList);
+    //console.log(sidebar.classList);
+    if(sidebar.classList.contains("show-sidebar")){
+        sidebar.classList.remove("show-sidebar");
+    }
+    else{
+        sidebar.classList.add("show-sidebar");
+    }
+});
+
+closeBtn.addEventListener("click", function(){
+    sidebar.classList.remove("show-sidebar");
 });
