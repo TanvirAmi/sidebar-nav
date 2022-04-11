4e53db5d3d43aaebf31a7de46abd8d6f67dc4a25
TanvirAmi <tanvir.focus@gmail.com>
2022-04-11 16:58:43 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const burgerMenu = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+burgerMenu.addEventListener("click", function(){
+    console.log(sidebar.classList);
+});
